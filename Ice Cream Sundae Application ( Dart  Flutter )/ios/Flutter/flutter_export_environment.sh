7937b4c3bfe0a5ec18aa76fc7528149935315977
Elouis12 <elouis12@me.com>
2022-04-22 02:21:03 -0400

    Add files via upload

---
+++ Ice Cream Sundae Application ( Dart  Flutter )/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\eloui\Downloads\INSTALLERS\Flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\eloui\OneDrive\Documents\FOLDERS\SEM\SEM6 BOOKS\MOBILE COMPUTING\Assignment_3"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
